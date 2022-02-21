33c123412884a295922af965f1552b5563090cc5
Angelica Hoskins <anghos080@gmail.com>
2022-02-21 17:40:43 -0600

    Updated project

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+/*
+
+Hosted Projects and Their Github Link: 
+    1) The Fresh Catch:
+        1. Website: https://ahos333.github.io/theFreshCatch.io/
+        2. Github: https://github.com/Ahos333/theFreshCatch.io
+    
+    2) E-cart
+        1. Website: https://ahos333.github.io/e-cart.io-gh-pages/
+        2. Github: https://github.com/Ahos333/e-cart.io-gh-pages
+    
+    3) BookApp
+        1. Website: https://ahos333.github.io/e-cart.io-gh-pages/
+        2. Github: https://github.com/Ahos333/e-cart.io-gh-pages
+    
+    4) 
+
+    5) DevHire
+        1. Website: https://ahos333.github.io/e-cart.io-gh-pages/
+        2. Github: https://github.com/Ahos333/e-cart.io-gh-pages
+
+
+*/
